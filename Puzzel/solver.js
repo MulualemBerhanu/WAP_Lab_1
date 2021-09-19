216c580242cfcf1f3bd940c20ca858236664c554
seidsid <seidsidseid@gmail.com>
2021-09-18 19:32:17 -0500

    solver added

---
+++ Puzzel/solver.js
@@ -0,0 +1,67 @@
+//accepts two dimensional array
+//returns coordinates
+function solve(maze){
+    let zeroPos=findBlankCord(maze);
+    for(let depth=1;;depth++){
+        let soln = solveIterativeDeepningDFS(maze,[],zeroPos,depth);
+        if(soln)return soln;
+    }
+}
+function findBlankCord(maze){
+    for(let i=0;i<maze.length;i++){
+        for(let j=0;j<maze[i].length;j++){
+            if(maze[i][j]==Number.MAX_VALUE)return {x:i,y:j};
+        }
+    }
+}
+function move(maze, pieceCord, targetCord){
+    maze=clone(maze);
+    maze[pieceCord.x][pieceCord.y]=maze[targetCord.x][targetCord.y];
+    maze[targetCord.x][targetCord.y]=Number.MAX_VALUE;
+    return maze;
+}
+function clone(maze){
+    return maze.map(oned=>oned.map(e=>e));
+}
+function availableMoves(blankCord, width, height){
+    let moves = [];
+    if(blankCord.x<height-1)moves.push({x:blankCord.x+1,y:blankCord.y});
+    if(blankCord.x>0)moves.push({x:blankCord.x-1,y:blankCord.y});
+    if(blankCord.y<width-1)moves.push({x:blankCord.x,y:blankCord.y+1});
+    if(blankCord.y>0)moves.push({x:blankCord.x,y:blankCord.y-1});
+    return moves;
+}
+
+function isSolved(maze){
+    for(let i=0,prev=0;i<maze.length;i++){
+        for(let j=0;j<maze[i].length;j++){
+            if(maze[i][j]<prev)return false;
+            prev=maze[i][j];
+        }
+    }
+    return true;
+}
+
+function solveIterativeDeepningDFS(maze,moves,blankCord,maxDepth,depth=1){
+    if(maxDepth<depth)return false;
+    else{
+        if(maxDepth===depth&&isSolved(maze))return moves;
+        else{
+            let availMoves=availableMoves(blankCord,maze[0].length,maze[0].length);
+            for(let amove of availMoves){
+                moves.push(amove);
+                let soln = solveIterativeDeepningDFS(move(maze,blankCord,amove), moves,amove, maxDepth, depth+1);
+                if(soln)return soln;
+                moves.pop();
+            }
+        }
+    }
+}
+
+let bl=Number.MAX_VALUE;
+let maze=[[1,bl,2,3],
+          [5,6,8,4],
+          [9,10,7,11],
+          [13,14,15,12]];
+
+console.log(solve(maze));
